b266e2c09efa250250bd8843eadd78f5d163a6ce
Rafael Figueiredo <rafael.figueiredo91@gmail.com>
2020-06-03 10:08:37 -0300

    list users

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,12 +3,35 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res)=>{
-    console.log("/")
-    res.json({hello:'world'})
+
+const users = [
+    'Rafael',
+    'Diego',
+    'Cleiton',
+    'Robson',
+    'Daniel',
+]
+
+app.get('/users', (request, response)=>{
+    console.log('Listagem de usuários')
+    const search = String(request.query.search).toLowerCase()
+    const filteredUsers = search ? users.filter(user => user.toLowerCase().includes(search)) : users
+    return response.json(filteredUsers)
 })
 
-const PORT = 5252
+app.get('/users/:id', (request, response)=>{
+    console.log("Listagem de usuários único")
+    const id = Number(request.params.id)
+    const user = users[id]
+    return response.json(user)
+})
+
+app.post('/user', (request, response)=>{
+    const {user} = request.body
+    users.push(user)
+})
+
+const PORT = process.env['PORT'] || 5252
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`)
 })
