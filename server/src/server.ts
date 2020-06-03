554e9890e1617bb4872d8703e2954eff04e3be81
Rafael Figueiredo <rafael.figueiredo91@gmail.com>
2020-06-03 14:52:58 -0300

    cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import routers from './router'
 import path from 'path'
-
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(routers)
