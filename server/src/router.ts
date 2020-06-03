a18bc04f871a9c0854580c32aedf996fe417c45d
Rafael Figueiredo <rafael.figueiredo91@gmail.com>
2020-06-03 13:39:29 -0300

    show method of itemsController

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,59 +1,15 @@
 
 import express from 'express'
 import knex from './database/connection'
-
+import PointsController from './controllers/pointsController'
+import ItemsController from './controllers/itemsController'
 
 const router = express.Router()
+const itemsController = new ItemsController();
 
-router.get('/items', async (request, response)=>{
-    const items = await knex('items').select('*');
 
-    const serializedItems = items.map(item=>{
-        return {
-            title: item.title,
-            image_url: `http://localhost:5252/uploads/${item.image}`
-        }
-    })
-    return response.json({
-        serializedItems
-    })
-})
 
-router.post('/points', async(request, response)=>{
-    const {
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    } = request.body;
+router.get('/items', itemsController.index)
 
-    const trx = await knex.transaction();
-
-    const insertedIds = await trx('points').insert({
-        name,
-        image: 'image-fake',
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-    })
-
-    const point_id = insertedIds[0]
-    const pointItems = items.map((item_id: number)=>{
-        return{
-            item_id,
-            point_id
-        }
-    })
-    await trx('point-items').insert(pointItems)
-
-    return response.json({success: true})
-})
 
 export default router
