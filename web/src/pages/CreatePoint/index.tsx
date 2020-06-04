d3daabb87fc0e9ca08096ae3d943d5cfbad90708
Rafael Figueiredo <rafael.figueiredo91@gmail.com>
2020-06-04 15:25:40 -0300

    buscando estados e cidade pela api do IBGE

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,8 +1,10 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, ChangeEvent} from 'react'
 import {Link} from 'react-router-dom'
 import {FiArrowLeft} from 'react-icons/fi'
 import {Map, TileLayer, Marker} from 'react-leaflet'
 import api from '../../services/api'
+
+import axios from 'axios'
 
 
 import './styles.css'
@@ -15,8 +17,20 @@ interface Item{
     image_url: string
 
 }
+
+interface IBGEUFResponse{
+    sigla: string
+}
+interface IBGECityResponse{
+    nome: string
+}
+
 const CreatePoint = ()=>{
     const [items, setItems] = useState<Item[]>([])
+    const [ufs, setUfs] = useState<string[]>([])
+    const [cities, setCities] = useState<string[]>([])
+    const [selectedUf, setSelectedUf] = useState<string>('0')
+    const [selectedCity, setSelectedCity] = useState<string>('0')
 
     useEffect(()=>{
         api.get('items')
@@ -24,6 +38,35 @@ const CreatePoint = ()=>{
                 setItems(response.data)
             })
     }, [])
+
+    useEffect(()=>{
+        axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+            .then(response =>{
+                const ufInitials = response.data.map(uf=>uf.sigla).sort()
+                setUfs(ufInitials)
+            })
+    }, [])
+
+    useEffect(()=>{
+        if(selectedUf ==='0') return
+        axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+            .then(response =>{
+                const citieNames = response.data.map(city=>city.nome).sort()
+                setCities(citieNames)
+            })
+
+    }, [selectedUf])
+
+
+    function handleSelectedUf(event: ChangeEvent<HTMLSelectElement>){
+        const uf = event.target.value
+        setSelectedUf(uf)
+    }
+
+    function handleSelectedCity(event: ChangeEvent<HTMLSelectElement>){
+        const city = event.target.value
+        setSelectedCity(city)
+    }
 
     return(
         <div id="page-create-point">
@@ -84,24 +127,42 @@ const CreatePoint = ()=>{
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
                         <Marker position={[-22.9071094,-43.1253929]} />
-                    </Map>/>
-                    <div className="filed-group">
+                    </Map>
+                    <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
-                            <select name="uf" id="uf">
+                            <select
+                                name="uf"
+                                id="uf"
+                                value={selectedUf}
+                                onChange={handleSelectedUf}
+                            >
+                                    
                                 <option value="0">Selecione uma UF</option>
+                                {ufs.map( uf=>(
+                                    <option
+                                        key={uf}
+                                        value={uf}
+                                    >{uf}</option>
+                                ))}
                             </select>
                         </div>
-                    </div>
-                    <div className="filed-group">
+                    
+                    
                         <div className="field">
                             <label htmlFor="cidade">Cidade</label>
-                            <select name="cidade" id="cidade">
+                            <select name="cidade" id="cidade" value={selectedCity} onChange={handleSelectedCity}>
                                 <option value="0">Selecione uma cidade</option>
+                                {cities.map( city=>(
+                                    <option
+                                        key={city}
+                                        value={city}
+                                    >{city}</option>
+                                ))}
                             </select>
                         </div>
+                    
                     </div>
-
 
                     <legend>
                         <h2>Ítens de coleta</h2>
